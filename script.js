15459e399ca687c4f8049bd1437b9d0f68307b64
CarlosPrieto <carlosprieto66@icloud.com>
2022-07-29 12:24:15 -0700

    function that generates divs and adjusts the rows and columns

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
 "use strict"
 
+// grid container
+const gridContainer = document.querySelector('.grid-container');
+const numOfPixels = gridContainer.childElementCount;
+gridContainer.style.border = '1px solid red';
+
+
+
+
+
+
+    function changeGridSize() {
+        
+       
+    }
+
+
+
+
+
+function createPixel(gridSize) {
+    gridContainer.setAttribute('style',
+    `grid-template-columns: repeat(${gridSize}, 10px); 
+    grid-template-rows: repeat(${gridSize}, 10px);`);
+
+
+    gridSize = gridSize*gridSize;
+    for (let i = 0; i < gridSize; i++) {
+         const div = document.createElement('div');
+
+            div.classList.add('pixel');
+            gridContainer.appendChild(div);  
+        
+    }
+
+   console.log(gridSize); 
+   return gridSize;
+};
+
+
+
+
+createPixel(32);
+
